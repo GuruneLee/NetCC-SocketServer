3104e300c75e9af2c528cf98bb14b56d74f48538
gurunelee <dlckdgk4858@gmail.com>
2020-11-17 23:21:00 +0900

    a

--- socket-server/main.go
+++ socket-server/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -14,27 +12,4 @@ func main() {
 		serveWs(s, w, r)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func RespJSON(w http.ResponseWriter, r bool, e error) {
-	var code int  //http status code
-	var es string //error string
-	if r == true {
-		code = http.StatusAccepted
-		es = ""
-	} else {
-		code = http.StatusUnauthorized
-		es = e.Error()
-	}
-
-	re := Resp{es}
-	result, err := json.Marshal(re)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(result)
 }
